12e7be28f6e22334cb127f533bfd52a1c790efbe
Rhinestone-Cowboy <diegopadilla005@gmail.com>
2025-10-25 13:43:15 -0700

    Create sheetmusic database format.

--- five_min_of_harmony/apps/SheetMusic/apps.py
+++ five_min_of_harmony/apps/SheetMusic/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MeasureConfig(AppConfig):
+class SheetMusicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'measure'
+    name = 'apps.SheetMusic'
